cee732dbb25bde4126fe0eaf0e464298861d4086
Estou-maker <eslemtouhemi@gmail.com>
2026-06-14 11:22:30 +0100

    update

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,20 @@ module.exports = defineConfig({
     USER_PASSWORD: '123',
   },
 
+  reporter: 'cypress-mochawesome-reporter',
+    video: true,
+      reporterOptions: {
+        charts: true,
+        ReportPageTitle: 'Stocky Results',
+        embeddedScreenshots: true,
+        inlineAssets: true,
+        saveAllAttempts: true,
+        html: true,
+        overwrite: true,
+        timestamp: 'mmddyyyy_HHMMss',
+        reportDir: 'cypress/Reports/Mochawsome-report',
+      },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
---
+++ pipeline.groovy
@@ -0,0 +1,63 @@
+pipeline {
+    agent any
+    
+    environment {
+        NODE_HOME = 'C:\\Program Files\\nodejs'
+        PATH = "${env.PATH};${NODE_HOME}"
+    }
+
+    stages {
+        stage('Verify Environment') {
+            steps {
+                echo 'Verifying Env Setup...'
+                bat 'node -v'
+                bat 'npm -v'
+                bat 'git --version'
+            }
+        }
+
+        stage('Clean Json Report') {
+            steps {
+                bat 'del /S /Q *.json'
+            }
+        }
+
+        stage('Checkout Code') {
+            steps {
+                echo 'Checking out code from github...'
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: '*/main']],
+                    userRemoteConfigs: [[
+                        url: 'https://github.com/Estou-maker/Stocky_automation_g29.git'
+                        credentialsId: 'TKNG29'
+                    ]]
+                ])
+            }
+        }
+
+        stage('Install dependencies') {
+            steps {
+                echo 'Installing Project Dependencies'
+                bat 'npm install'
+            }
+        }
+
+        stage('Create Reports Dir') {
+            steps {
+                bat '''
+                if not exist "cypress\\reports\\.json" mkdir "cypress\\reports\\.json"
+                if not exist "cypress\\reports\\html_reports" mkdir "cypress\\reports\\html_reports"
+                '''
+            }
+        }
+
+        stage('Run Cypress Tests') {
+            steps {
+                echo 'Running Tests in headless Mode'
+                bat 'npx Cypress run --headless --browser chrome'
+            }
+        }
+
+    }
+}
